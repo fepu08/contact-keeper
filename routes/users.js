eb972c24227f0682c2a15c9f266432210d73ba46
fepu08 <ferencbesenyei@protonmail.com>
2021-02-05 14:23:05 +0100

    init jwt for register

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -15,7 +17,7 @@ router.post(
     check('email').isEmail().withMessage('Please include a valid email'),
     check('password')
       .isLength({ min: 6 })
-      .withMessage('Please enter a password with 6 or more characters'),
+      .withMessage('Please enter a password with 6 or more characters')
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -35,14 +37,30 @@ router.post(
       user = new User({
         name,
         email,
-        password,
+        password
       });
 
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(user.password, salt);
 
       await user.save();
-      res.send('user saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000 // set it 3600 in deploy
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
