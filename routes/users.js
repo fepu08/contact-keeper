6c50176faac9c34e9b639835434b6c5acc7d17c3
fepu08 <ferencbesenyei@protonmail.com>
2021-02-05 14:15:49 +0100

    create user with request, hash pw with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -16,12 +17,36 @@ router.post(
       .isLength({ min: 6 })
       .withMessage('Please enter a password with 6 or more characters'),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.json({ msg: 'passed' });
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email }); // same as email:email
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // because all the fields are the same name as the variable (e.g. name: name) we can use the ES6 syntax and write only the name of the field
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+
+      await user.save();
+      res.send('user saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
