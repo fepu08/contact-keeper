b2b51ccf14e1039d31bb667cc0650f5d6bb1e862
fepu08 <ferencbesenyei@protonmail.com>
2021-02-05 14:06:20 +0100

    register user validation w express-validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @acc     public
-router.post('/', (req,res) => {
-    res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name').notEmpty().withMessage('Name is required'),
+    check('email').isEmail().withMessage('Please include a valid email'),
+    check('password')
+      .isLength({ min: 6 })
+      .withMessage('Please enter a password with 6 or more characters'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.json({ msg: 'passed' });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
-const app  = express();
+const app = express();
 
 connectDB();
+
+app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
@@ -12,5 +14,5 @@ app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
