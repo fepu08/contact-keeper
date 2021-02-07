079647de8f0714b4edfbac5d5e99fd401d5bfccd
fepu08 <ferencbesenyei@protonmail.com>
2021-02-07 17:18:07 +0100

    Filter contacts

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
+import ContactFilter from '../contacts/ContactFilter';
+import ContactContext from '../../context/contact/contactContext';
 
 const Home = () => {
+  const contactContext = useContext(ContactContext);
+
   return (
     <div className='grid-2'>
       <div>
         <ContactForm />
       </div>
       <div>
+        {contactContext.contacts.length !== 0 && <ContactFilter />}
         <Contacts />
       </div>
     </div>
