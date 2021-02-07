86db7bdc651c150e588ac3b86d5300deffe630f5
fepu08 <ferencbesenyei@protonmail.com>
2021-02-07 12:54:19 +0100

    delete contact from UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,9 @@ const ContactState = (props) => {
   };
 
   // Update Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Delete Contact
 
@@ -63,7 +66,8 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
