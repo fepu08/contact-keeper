44007bcb1abec84325f7d53c3b7b0267632eea1d
fepu08 <ferencbesenyei@protonmail.com>
2021-02-17 20:23:09 +0100

    Fix text

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -42,7 +42,7 @@ const Login = (props) => {
   return (
     <div className='form-container'>
       <h1>
-        Account <span className='text-primary'>Register</span>
+        <span className='text-primary'>Login</span>
       </h1>
       <form onSubmit={onSubmit}>
         <div className='form-group'>
